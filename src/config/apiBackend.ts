778e9a097259955288f029aa6361dc1dda2a7eb6
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-25 18:49:19 -0300

    fix: :bug: The "/" is repeated in the url to the bakend api

--- src/config/apiBackend.ts
+++ src/config/apiBackend.ts
@@ -1,6 +1,6 @@
 export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
 
-export const API_BACKEND_URL = BACKEND_URL + '/api/';
+export const API_BACKEND_URL = BACKEND_URL + '/api';
 
 // ROUTES
 const API_PATH_USER = '/auth';
