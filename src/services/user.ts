162f44aa70ca496f1f640a938a307d4860867612
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-06-25 18:46:54 -0300

    fix: :bug: Error: UserServices method generateCode - Http verb wrong, from "get" to "post"

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 import { API_BACKEND_URL, API_PATH_USER_CODE, API_PATH_USER_LOGIN } from '../config';
 
-import type { IErrorBackendApi } from '../types/apiBackend.interface';
 import { errorApiBackend } from './utils';
 
 export class UserServices {
@@ -11,11 +10,11 @@ export class UserServices {
 		headers: { Accept: 'application/json' },
 	});
 
-	static generateCode = async (email: string): Promise<{ message: string } | IErrorBackendApi> => {
+	static generateCode = async (email: string) => {
+		const data = { email };
+
 		try {
-			const res = await this.axiosInstance.get<{ message: string }>(API_PATH_USER_CODE, {
-				data: { email },
-			});
+			const res = await this.axiosInstance.post<{ message: string; newCode: string }>(API_PATH_USER_CODE, data);
 			return res.data;
 		} catch (err) {
 			return errorApiBackend(err);
@@ -27,9 +26,7 @@ export class UserServices {
 
 		try {
 			// ! Tengo que tener cuidado, ya que no se si enviar el token en la respuesta o como cookie
-			const res = await this.axiosInstance.post<{ message: string; token: string }>(API_PATH_USER_LOGIN, {
-				data,
-			});
+			const res = await this.axiosInstance.post<{ message: string; token: string }>(API_PATH_USER_LOGIN, data);
 			return res.data;
 		} catch (err) {
 			return errorApiBackend(err);
