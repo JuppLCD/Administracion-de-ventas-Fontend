474d3e2c0c58a2830448d71cd95d41ca1456d123
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-01 13:00:53 -0300

    feat: :lipstick: Adding styles to the body and modifying the styles to the Layout component

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,5 +1,5 @@
 import { Container } from '@chakra-ui/react';
 
 export default function Layout({ ...promps }) {
-	return <Container maxW='container.lg' centerContent mt={20} {...promps} />;
+	return <Container maxW='container.md' centerContent bg='white' rounded='2xl' shadow='base' p={5} {...promps} />;
 }
