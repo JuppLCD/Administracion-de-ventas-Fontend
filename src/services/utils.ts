ffb795f0bb2289f4f40f44038bd9becb4e3f6eff
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-05 15:02:50 -0300

    fix: :bug: Fixing the errorApiBackend utility so that it returns an error correctly

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -4,13 +4,21 @@ import { IErrorBackendApi } from '../types/apiBackend.interface';
 export function errorApiBackend(err: unknown) {
 	console.log(err);
 
-	if (isAxiosError(err)) {
-		return err.response?.data as IErrorBackendApi;
+	if (isAxiosError(err) && err.response?.data) {
+		return err.response.data as IErrorBackendApi;
+	}
+
+	if (isAxiosError(err) && err.message === 'Network Error') {
+		return {
+			message: 'Network Error',
+			error: 'Error trying to connect to server',
+			statusCode: 503,
+		} as IErrorBackendApi;
 	}
 
 	return {
 		message: 'Error desconocido',
-		error: 'A ocurrido un error al hacer login',
+		error: 'A ocurrido un error',
 		statusCode: 0,
 	} as IErrorBackendApi;
 }
