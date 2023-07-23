0edce71648ce2e5e7891b10badd18952c5c62298
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-07-23 17:42:51 -0300

    refactor: :recycle: Modifying the backend api constants

--- src/config/apiBackend.ts
+++ src/config/apiBackend.ts
@@ -2,9 +2,17 @@ export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
 
 export const API_BACKEND_URL = BACKEND_URL + '/api';
 
+// BASIC
+export const STORE_PATH = '/store';
+
 // ROUTES
-const API_PATH_USER = '/auth';
+export const API_PATH_USER = API_BACKEND_URL + '/auth';
+export const API_PATH_PRODUCT = API_BACKEND_URL + '/product';
 
 // USER
-export const API_PATH_USER_CODE = API_PATH_USER + '/code';
-export const API_PATH_USER_LOGIN = API_PATH_USER + '/login';
+export const API_PATH_USER_VALIDATE_TOKEN = API_PATH_USER + '/validate_token';
+export const API_PATH_USER_CODE = '/code';
+export const API_PATH_USER_LOGIN = '/login';
+
+// Product
+export const API_PATH_PRODUCT_GET_BY_CATEGORY = '/category';
