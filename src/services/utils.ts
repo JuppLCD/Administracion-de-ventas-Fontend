c1a9d403cca31737e0f5311d24e87d614b4cfc8a
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-08-07 22:03:52 -0300

    feat: :sparkles: The "ErrorBackendApi" class was created so that the "errorApiBackend" utility returns the error as said object.

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -2,23 +2,31 @@ import { isAxiosError } from 'axios';
 import { IErrorBackendApi } from '@/types/backend/error.interface';
 
 export function errorApiBackend(err: unknown) {
-	console.log(err);
+	console.log('Ocurrio un error (errorApiBackend) -|- ', err);
 
 	if (isAxiosError(err) && err.response?.data) {
-		return err.response.data as IErrorBackendApi;
+		return new ErrorBackendApi(err.response.data as IErrorBackendApi);
 	}
 
 	if (isAxiosError(err) && err.message === 'Network Error') {
-		return {
+		return new ErrorBackendApi({
 			message: 'Network Error',
 			error: 'Error trying to connect to server',
 			statusCode: 503,
-		} as IErrorBackendApi;
+		});
 	}
 
-	return {
-		message: 'Error desconocido',
-		error: 'A ocurrido un error',
-		statusCode: 0,
-	} as IErrorBackendApi;
+	return new ErrorBackendApi({});
+}
+
+export class ErrorBackendApi implements IErrorBackendApi {
+	message: string;
+	error: string;
+	statusCode: number;
+
+	constructor({ message, error, statusCode }: Partial<IErrorBackendApi>) {
+		this.message = message ?? 'Error desconocido';
+		this.error = error ?? 'A ocurrido un error';
+		this.statusCode = statusCode ?? 0;
+	}
 }
